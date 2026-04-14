c909df58fdb591bb34cb65ec46fd3f8e0a3a2200
nabinkrpal <138757521+nabinkrpal@users.noreply.github.com>
2026-04-14 20:46:35 +0530

    Update App.js

--- stress-frontend/src/App.js
+++ stress-frontend/src/App.js
@@ -1,4 +1,77 @@
 
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Dashboard from "./pages/Dashboard";
+// import History from "./pages/History";
+// import ResetPassword from "./pages/ResetPassword";
+// import ProtectedRoute from "./components/ProtectedRoute";
+
+
+
+
+// function App() {
+
+//   // Check authentication
+//   const isAuthenticated = localStorage.getItem("token");
+
+//   return (
+//     <Router>
+
+//       <Routes>
+
+//         {/* Public Routes */}
+
+//         <Route
+//           path="/"
+//           element={
+//             isAuthenticated ? <Navigate to="/dashboard" /> : <Login />
+//           }
+//         />
+
+//         <Route
+//           path="/register"
+//           element={
+//             isAuthenticated ? <Navigate to="/dashboard" /> : <Register />
+//           }
+//         />
+
+//         {/* Protected Routes */}
+
+//         <Route
+//           path="/dashboard"
+//           element={
+//             <ProtectedRoute>
+//               <Dashboard />
+//             </ProtectedRoute>
+//           }
+//         />
+
+//         <Route
+//           path="/history"
+//           element={
+//             <ProtectedRoute>
+//               <History />
+//             </ProtectedRoute>
+//           }
+//         />
+//         <Route path="/reset-password" element={<ResetPassword />} />
+        
+
+//         {/* Fallback Route */}
+
+//         <Route path="*" element={<Navigate to="/" />} />
+
+//       </Routes>
+
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
@@ -8,7 +81,7 @@ import Dashboard from "./pages/Dashboard";
 import History from "./pages/History";
 import ResetPassword from "./pages/ResetPassword";
 import ProtectedRoute from "./components/ProtectedRoute";
-
+import AboutFeedback from "./pages/AboutFeedback";
 
 
 
@@ -58,7 +131,14 @@ function App() {
           }
         />
         <Route path="/reset-password" element={<ResetPassword />} />
-        
+        <Route
+          path="/about-feedback"
+          element={
+            <ProtectedRoute>
+              <AboutFeedback />
+            </ProtectedRoute>
+          }
+        />
 
         {/* Fallback Route */}
 
